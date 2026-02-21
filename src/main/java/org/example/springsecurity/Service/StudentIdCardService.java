332e5ca8c3403d5b0eaab2ce15ab8ee801565ebd
premanshu <premjha@lentra.ai>
2026-02-21 17:07:43 +0530

    used a function to get student id card and student

--- src/main/java/org/example/springsecurity/Service/StudentIdCardService.java
+++ src/main/java/org/example/springsecurity/Service/StudentIdCardService.java
@@ -6,9 +6,6 @@ import org.example.springsecurity.Model.StudentIdCardRepository;
 import org.example.springsecurity.Model.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-import java.util.Optional;
-
 public class StudentIdCardService {
     @Autowired
     StudentIdCardRepository studentIdCardRepository;
@@ -16,21 +13,27 @@ public class StudentIdCardService {
     @Autowired
     StudentRepository studentRepository;
 
+    public Student getStudent(Long studentId){
+        return studentRepository.findById(studentId).orElseThrow(()->new RuntimeException("Student not found"));
+    }
+    public StudentIdCard getStudentIdCard(Long id){
+       return studentIdCardRepository.findByStudentId(id).orElseThrow(()->new RuntimeException("Id card not found for the student"));
+    }
     public StudentIdCard getIdCardsByStudentId(Long studentId){
-        Student student = studentRepository.findById(studentId).orElseThrow(()->new RuntimeException("Student not found"));
-        StudentIdCard idCard = studentIdCardRepository.findByStudentId(student.getId()).orElseThrow(()->new RuntimeException("Id card not found for the student"));
+        Student student = getStudent(studentId);
+        StudentIdCard idCard = getStudentIdCard(student.getId());
         return idCard;
     }
 
     public void addIdCard(Long studentId,StudentIdCard idCard){
-         Student student = studentRepository.findById(studentId).orElseThrow(()->new RuntimeException("Student not found"));
+         Student student = getStudent(studentId);
          idCard.setStudent(student);
          studentIdCardRepository.save(idCard);
     }
 
     public void updateIdCard(Long studentId,Long id,StudentIdCard idCard){
-        Student student = studentRepository.findById(studentId).orElseThrow(()->new RuntimeException("Student not found"));
-        StudentIdCard studentIdCard = studentIdCardRepository.findByStudentId(student.getId()).orElseThrow(()->new RuntimeException("Id card not found for the student"));
+        Student student = getStudent(studentId);
+        StudentIdCard studentIdCard = getStudentIdCard(student.getId());
         if(idCard.getCardNumber() != null) studentIdCard.setCardNumber(idCard.getCardNumber());
         studentIdCardRepository.save(studentIdCard);
     }
